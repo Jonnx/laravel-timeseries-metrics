c7b912cccd61b7464a56790c5d6a090671728672
Jonas Weigert <weigert.jonas@gmail.com>
2018-01-26 22:33:33 +0000

    package scaffolding

---
+++ src/LaravelTimeseriesMetricsServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Jonnx\LaravelTimeseriesMetrics;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelTimeseriesMetricsServiceProvider extends ServiceProvider {
+    
+	public function boot() {
+		$this->publishes([
+			__DIR__.'/../config/metrics.php' => config_path('metrics.php'),
+		], 'metrics');
+	}
+	
+    public function register() {
+		$this->mergeConfigFrom( __DIR__.'/../config/metrics.php', 'metrics');
+    }
+    
+    public function provides() {
+        return [];
+    }
+}

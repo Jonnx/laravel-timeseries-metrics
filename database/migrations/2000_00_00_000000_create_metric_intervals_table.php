8ca9984384d6e5e0f7bd85c2aa0a290683d51221
Jonas Weigert <weigert.jonas@gmail.com>
2018-01-27 00:09:36 +0000

    fix migration file

--- database/migrations/2000_00_00_000000_create_metric_intervals_table.php
+++ database/migrations/2000_00_00_000000_create_metric_intervals_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateMetricsIntervalsTable extends Migration
+class CreateMetricIntervalsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -29,6 +29,6 @@ class CreateMetricsIntervalsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('metrics_intervals');
+        Schema::dropIfExists('metric_intervals');
     }
 }
